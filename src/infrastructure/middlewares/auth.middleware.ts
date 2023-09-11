0dfa936b44556a16618eb806a168e20c08b1a5de
Alireza Naghdipour <alirezanqp@gmail.com>
2023-09-11 21:14:57 +0330

    add auth middleware

---
+++ src/infrastructure/middlewares/auth.middleware.ts
@@ -0,0 +1,31 @@
+import { ExpressMiddlewareInterface } from 'routing-controllers';
+import { Service } from 'typedi';
+import * as jwt from 'jsonwebtoken';
+import { NextFunction, Response } from 'express';
+import { authConfig } from '../../config';
+
+interface DecodedToken {
+  id: number;
+}
+
+@Service()
+export class AuthCheck implements ExpressMiddlewareInterface {
+  use(request: any, response: Response, next: NextFunction) {
+    const token = request.cookies.token;
+
+    if (!token) {
+      return response
+        .status(401)
+        .json({ message: 'Access denied. No token provided.' });
+    }
+
+    try {
+      const decodedToken = jwt.verify(token, authConfig.secret) as DecodedToken;
+
+      request.user = { id: decodedToken.id };
+      next();
+    } catch (error) {
+      return response.status(400).json({ message: 'Invalid token.' });
+    }
+  }
+}
