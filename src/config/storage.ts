db9dc4435a1fbc8dd8e9d4f7246bf0729ab30e3c
Alireza Naghdipour <alirezanqp@gmail.com>
2023-09-25 11:26:30 +0330

    add storage config

--- src/config/storage.ts
+++ src/config/storage.ts
@@ -5,5 +5,7 @@ export const storageConfig = {
   port: env('STORAGE_PORT'),
   accessKey: env('STORAGE_ACCESS_KEY'),
   secretKey: env('STORAGE_SECRET_KEY'),
-  bucketName: env('BUCKET_NAME')
+  bucketName: env('BUCKET_NAME'),
+  region: env('STORAGE_REGION', 'US-East'),
+  baseS3Path: env('BASE_STORAGE_PATH', 'users/')
 };
